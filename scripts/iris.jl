167c72203fc8e2c1685b7e45d3700b130e20fe1e
Dennis Wilson <dennis@d9w.xyz>
2020-01-31 13:28:46 +0100

    iris works, mnist unable to do binary classification reliably

--- scripts/iris.jl
+++ scripts/iris.jl
@@ -18,12 +18,25 @@ end
 
 X, Y = data_setup()
 
+function evaluate(ind::MTCGP.MTCGPInd)
+    accuracy = 0.0
+    for i in 1:size(X, 2)
+        out = process(ind, X[:, i])
+        if argmax(out) == argmax(Y[:, i])
+            accuracy += 1
+        end
+    end
+    [accuracy / size(X, 1)]
+end
+
 e = Darwin.Evolution(MTCGPInd, cfg; id="iris")
 mutation = i::MTCGPInd->goldman_mutate(cfg, i)
 e.populate = x::Darwin.Evolution->Darwin.oneplus_populate!(
     x; mutation=mutation)
-e.evaluate = x::Darwin.Evolution->Darwin.lexicase_evaluate!(
-    x, X, Y, MTCGP.interpret)
+e.evaluate = x::Darwin.Evolution->Darwin.fitness_evaluate!(
+    x; fitness=evaluate)
+#e.evaluate = x::Darwin.Evolution->Darwin.lexicase_evaluate!(
+#    x, X, Y, MTCGP.interpret)
 
 Darwin.run!(e)
 best = sort(e.population)[end]
--- scripts/mnist.jl
+++ scripts/mnist.jl
@@ -14,27 +14,70 @@ function data_setup()
     end
     r = train_y
     inds = r .<= 1
+    #Y = zeros(maximum(r)+1, size(X, 2))
     Y = zeros(maximum(r)+1, size(X, 2))
     for i in eachindex(r)
         Y[r[i]+1, i] = 1.0
     end
-    X[:, inds], Y[:, inds]
+    X[:, inds], Y[1:2, inds]
 end
 
 X, Y = data_setup()
 
+function evaluate(ind::MTCGP.MTCGPInd)
+    accuracy = 0.0
+    data_inds = Random.randperm(size(X, 2))
+    for i in 1:100
+        out = MTCGP.mean_process(ind, X[:, data_inds[i]])
+        if argmax(out) == argmax(Y[:, data_inds[i]])
+            accuracy += 1
+        end
+    end
+    accuracy
+end
+
 # test_x,  test_y  = MLDatasets.MNIST.testdata()
 
-seed = 101
+seed = 200
 Random.seed!(seed)
 
-e = Darwin.Evolution(MTCGPInd, cfg; id=string("mnist", seed))
-mutation = i::MTCGPInd->goldman_mutate(cfg, i)
-e.populate = x::Darwin.Evolution->Darwin.oneplus_populate!(
-    x; mutation=mutation)
-e.evaluate = x::Darwin.Evolution->Darwin.lexicase_evaluate!(
-    x, X, Y, MTCGP.mean_interpret; seed=seed*x.gen, verify_best=true)
+n_evos = 20
+evolutions = Array{Darwin.Evolution}(undef, n_evos)
+experts = Array{MTCGPInd}(undef, n_evos)
+for i in eachindex(experts)
+    experts[i] = MTCGPInd(cfg)
+end
 
-Darwin.run!(e)
-best = sort(e.population)[end]
-println("Final fitness: ", best.fitness[1])
+function exchange_experts(e::Darwin.Evolution; n_experts=5)
+    append!(e.population, experts[Random.randperm(n_evos)[1:n_experts]])
+    # append!(e.population, sort(experts; rev=true)[1:n_experts])
+    println([i.fitness[1] for i in e.population])
+end
+
+for evo in eachindex(evolutions)
+    e = Darwin.Evolution(MTCGPInd, cfg; id=string("mnist", seed+evo),
+                         generation=exchange_experts)
+    mutation = i::MTCGPInd->goldman_mutate(cfg, i)
+    e.populate = x::Darwin.Evolution->Darwin.oneplus_populate!(
+        x; mutation=mutation)
+    #e.evaluate = x::Darwin.Evolution->Darwin.fitness_evaluate!(
+    #    x; fitness=evaluate)
+    e.evaluate = x::Darwin.Evolution->Darwin.lexicase_evaluate!(
+        x, X, Y, MTCGP.mean_interpret; seed=(seed+evo)*x.gen, verify_best=false)
+    evolutions[evo] = e
+end
+
+for step in 1:cfg["n_gen"]
+    best_fit = 0.0
+    for evo in eachindex(evolutions)
+        Darwin.step!(evolutions[evo])
+        experts[evo] = copy(sort(evolutions[evo].population)[end])
+        if experts[evo].fitness[1] > best_fit
+            best_fit = experts[evo].fitness[1]
+        end
+    end
+    println(step, " ", best_fit)
+    # Darwin.run!(e)
+    # best = sort(e.population)[end]
+    # println("Final fitness: ", best.fitness[1])
+end
